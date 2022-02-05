d125862783a6a6e2f1717ba0c2307c46f12a5277
Aleksandr Zdanovich <alex.zdns@gmail.com>
2022-02-05 12:45:29 +0700

    Add support viewBinding

--- app/src/main/java/ru/zdanovich/developerslife/MainActivity.kt
+++ app/src/main/java/ru/zdanovich/developerslife/MainActivity.kt
@@ -2,10 +2,14 @@ package ru.zdanovich.developerslife
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.zdanovich.developerslife.databinding.ActivityMainBinding
+import ru.zdanovich.developerslife.extensions.viewBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by viewBinding(ActivityMainBinding::inflate)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
