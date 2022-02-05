a3b3440cf9332c9abb1d2ad8c4208eb11feb78aa
Aleksandr Zdanovich <alex.zdns@gmail.com>
2022-02-05 16:58:51 +0700

    Add Hilt

---
+++ app/src/main/java/ru/zdanovich/developerslife/DevelopersLifeApplication.kt
@@ -0,0 +1,8 @@
+package ru.zdanovich.developerslife
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class DevelopersLifeApplication: Application() {
+}
--- app/src/main/java/ru/zdanovich/developerslife/MainActivity.kt
+++ app/src/main/java/ru/zdanovich/developerslife/MainActivity.kt
@@ -2,9 +2,12 @@ package ru.zdanovich.developerslife
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
 import ru.zdanovich.developerslife.databinding.ActivityMainBinding
 import ru.zdanovich.developerslife.extensions.viewBinding
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private val binding by viewBinding(ActivityMainBinding::inflate)
 
